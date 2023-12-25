85bdc13e6d0a2cf0d8db04ae5e5a015d582ac82d
Unxeen <148867773+Unxeen@users.noreply.github.com>
2023-12-25 12:25:38 +0100

    deprecated commit

--- Meta front end certi/react-app/src/App.jsx
+++ Meta front end certi/react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App(props) {
   const [count, setCount] = useState(0)
 
   return (
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {props.name} and {props.lots} is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
